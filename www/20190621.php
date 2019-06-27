c0468a38b7c342d59133f2230e59822cde7e0fa2
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-27 10:11:10 +0900

    Update 20190621.php

--- www/20190621.php
+++ www/20190621.php
@@ -26,8 +26,34 @@ function func_20190621b($mu_, $file_name_blog_) {
     error_log(print_r($match, true));
     $url = 'http://hyogo.rivercam.info' . $match[1];
     $res = $mu_->get_contents($url);
-    header('content-type: image/jpeg');
-    echo $res;
+    $description = '<img src="data:image/jpeg;base64,' . base64_encode($res) . '" />';
+    $description = '<![CDATA[' . $description . ']]>';
+    
+    $xml_text = <<< __HEREDOC__
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0">
+<channel>
+<title>River Now</title>
+<link>http://dummy.local/</link>
+<description>River Now</description>
+<item>
+<guid isPermaLink="false">__HASH__</guid>
+<pubDate>__PUBDATE__</pubDate>
+<title>River Now</title>
+<link>http://dummy.local/</link>
+<description>__DESCRIPTION__</description>
+</item>
+</channel>
+</rss>
+__HEREDOC__;
+
+    $xml_text = str_replace('__DESCRIPTION__', $description, $xml_text);
+    $xml_text = str_replace('__PUBDATE__', date('D, j M Y G:i:s +0900', strtotime('+9 hours')), $xml_text);
+    $xml_text = str_replace('__HASH__', hash('sha256', $description), $xml_text);
+    $file_name = '/tmp/' . getenv('FC2_RSS_05') . '.xml';
+    file_put_contents($file_name, $xml_text);
+    $mu_->upload_fc2($file_name);
+    unlink($file_name);
 }
 
 function func_20190621($mu_, $file_name_blog_) {
