5f81bd2ce42e2c576b632fe1ab52004fcab2f441
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-27 15:42:07 +0900

    Update 20190621.php

--- www/20190621.php
+++ www/20190621.php
@@ -21,39 +21,7 @@ function func_20190621b($mu_, $file_name_blog_) {
     $url = 'http://hyogo.rivercam.info/nishinomiya/detail/mukogawanamaze.html?' . hash('md5', microtime(true));
     $res = $mu_->get_contents($url);
     
-    // error_log($res);
-    $rc = preg_match('/<img alt="最新監視カメラ画像".+? src="(.+?)"/s', $res, $match);
-    error_log(print_r($match, true));
-    $url = 'http://hyogo.rivercam.info' . $match[1];
-    $res = $mu_->get_contents($url);
-    $description = '<img src="data:image/jpeg;base64,' . base64_encode($res) . '" />';
-    $description = '<![CDATA[' . $description . ']]>';
-    
-    $xml_text = <<< __HEREDOC__
-<?xml version="1.0" encoding="utf-8"?>
-<rss version="2.0">
-<channel>
-<title>River Now</title>
-<link>http://dummy.local/</link>
-<description>River Now</description>
-<item>
-<guid isPermaLink="false">__HASH__</guid>
-<pubDate>__PUBDATE__</pubDate>
-<title>River Now</title>
-<link>http://dummy.local/</link>
-<description>__DESCRIPTION__</description>
-</item>
-</channel>
-</rss>
-__HEREDOC__;
-
-    $xml_text = str_replace('__DESCRIPTION__', $description, $xml_text);
-    $xml_text = str_replace('__PUBDATE__', date('D, j M Y G:i:s +0900', strtotime('+9 hours')), $xml_text);
-    $xml_text = str_replace('__HASH__', hash('sha256', $description), $xml_text);
-    $file_name = '/tmp/' . getenv('FC2_RSS_05') . '.xml';
-    file_put_contents($file_name, $xml_text);
-    $mu_->upload_fc2($file_name);
-    unlink($file_name);
+    error_log($res);
 }
 
 function func_20190621($mu_, $file_name_blog_) {
