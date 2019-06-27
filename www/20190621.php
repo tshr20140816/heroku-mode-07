25f5765db875a632185c771825586a763317216d
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-27 09:39:08 +0900

    Update 20190621.php

--- www/20190621.php
+++ www/20190621.php
@@ -9,13 +9,20 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-func_20190621($mu, '/tmp/20190621dummy');
+func_20190621b($mu, '/tmp/20190621dummy');
 @unlink('/tmp/20190621dummy');
 
 $time_finish = microtime(true);
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 exit();
+
+function func_20190621b($mu_, $file_name_blog_) {
+    $url = 'http://hyogo.rivercam.info/nishinomiya/detail/mukogawanamaze.html';
+    $res = $mu_->get_contents($url);
+    
+    error_log($res);
+}
 
 function func_20190621($mu_, $file_name_blog_) {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
