8dbc986afd41f97c4d1f31f71beee3c20043edf7
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-03 13:31:27 +0900

    Update check_train.php

--- www/check_train.php
+++ www/check_train.php
@@ -92,7 +92,9 @@ function check_train($mu_)
             $tmp = trim(strip_tags($item));
             $tmp = preg_replace('/\t+/', '', $tmp);
             $tmp = mb_convert_kana($tmp, 'as');
-            $description .= "\n\n" . $tmp;
+            if (mb_strpos($tmp, '【芸備線】 西日本豪雨に伴う 運転見合わせ') == false) {
+                $description .= "\n\n" . $tmp;
+            }
         }
     }
 
