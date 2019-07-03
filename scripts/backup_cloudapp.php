8651340f1e0e222251fd725755b4791137f72623
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-03 11:50:34 +0900

    Update backup_cloudapp.php

--- scripts/backup_cloudapp.php
+++ scripts/backup_cloudapp.php
@@ -4,7 +4,7 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $pid = getmypid();
 $time_start = microtime(true);
-error_log("${pid} START scripts/update_ttrss.php " . date('Y/m/d H:i:s'));
+error_log("${pid} START scripts/backup_cloudapp.php " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
