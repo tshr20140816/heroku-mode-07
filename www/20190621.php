62bcb31ace87350ff42b76b5738d3f5c961f0b38
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-08 15:46:44 +0900

    Update 20190621.php

--- www/20190621.php
+++ www/20190621.php
@@ -25,16 +25,19 @@ function func_20190621($mu_)
     $tmp = explode('</script>', $res);
     $tmp = trim(end($tmp));
     error_log(strlen($tmp));
+    error_log($tmp);
     $tmp = json_decode($tmp);
     error_log(json_last_error());
     error_log(json_last_error_msg());
     error_log(print_r($tmp, true));
     
+    
+    /*
     $url = 'https://traininfo.jr-central.co.jp/shinkansen/var/train_info/train_location_info.json';
     $res = $mu_->get_contents_proxy($url);
     $tmp = explode('</script>', $res);
     $tmp = json_decode(trim(end($tmp)), true);
     error_log(print_r($tmp, true));
-    
+    */
 }
 
