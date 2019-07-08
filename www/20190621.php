2d0ea60e35ed1935f4667fea8a77b28be16ad050
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-08 14:40:30 +0900

    Update 20190621.php

--- www/20190621.php
+++ www/20190621.php
@@ -20,21 +20,20 @@ function func_20190621($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
     
-    /*
     $url = 'https://traininfo.jr-central.co.jp/shinkansen/pc/ja/ti08.html';
-    $res = $mu_->get_contents($url, $options);
+    $res = $mu_->get_contents_proxy($url, $options);
+    error_log($res);
     
     $url = 'https://traininfo.jr-central.co.jp/shinkansen/common/data/common_ja.json';
-    $res = $mu_->get_contents($url, $options);
-    */
+    $res = $mu_->get_contents_proxy($url, $options);
+    error_log($res);
     
     $url = 'https://traininfo.jr-central.co.jp/shinkansen/var/train_info/train_location_info.json';
     // $res = $mu_->get_contents($url, $options);
     $res = $mu_->get_contents_proxy($url);
     // error_log($res);
-    $tmp = trim(end(explode('</script>', $res)));
-    error_log($tmp);
-    error_log(print_r(json_decode($tmp, true), true));
+    $tmp = json_decode(trim(end(explode('</script>', $res))), true);
+    error_log(print_r($tmp, true));
     // error_log(json_decode(trim(end(explode('</script>', $res))), true));
     
 }
