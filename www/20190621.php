5409f277c84a7e7542c95c4e401d355c2fcadb61
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-08 14:43:11 +0900

    Update 20190621.php

--- www/20190621.php
+++ www/20190621.php
@@ -32,7 +32,8 @@ function func_20190621($mu_)
     // $res = $mu_->get_contents($url, $options);
     $res = $mu_->get_contents_proxy($url);
     // error_log($res);
-    $tmp = json_decode(trim(end(explode('</script>', $res))), true);
+    $tmp = explode('</script>', $res);
+    $tmp = json_decode(trim(end($tmp)), true);
     error_log(print_r($tmp, true));
     // error_log(json_decode(trim(end(explode('</script>', $res))), true));
     
