fa05e80463e58ca25ed5a6303379d6b6a6544522
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-08 14:30:22 +0900

    Update 20190621.php

--- www/20190621.php
+++ www/20190621.php
@@ -19,28 +19,7 @@ exit();
 function func_20190621($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
-
-    $cookie = tempnam("/tmp", 'cookie_' . md5(microtime(true)));
     
-    $options = [
-        CURLOPT_ENCODING => 'gzip, deflate, br',
-        CURLOPT_HTTPHEADER => [
-            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
-            'Cache-Control: no-cache',
-            'Connection: keep-alive',
-            'DNT: 1',
-            'Upgrade-Insecure-Requests: 1',
-            ],
-        CURLOPT_COOKIEJAR => $cookie,
-        CURLOPT_COOKIEFILE => $cookie,
-    ];
-    
-    /*
-    $url = 'https://jr-central.co.jp/';
-    $res = $mu_->get_contents_proxy($url);
-    error_log($res);
-    */
     /*
     $url = 'https://traininfo.jr-central.co.jp/shinkansen/pc/ja/ti08.html';
     $res = $mu_->get_contents($url, $options);
@@ -52,8 +31,8 @@ function func_20190621($mu_)
     $url = 'https://traininfo.jr-central.co.jp/shinkansen/var/train_info/train_location_info.json';
     // $res = $mu_->get_contents($url, $options);
     $res = $mu_->get_contents_proxy($url);
-    error_log($res);
+    // error_log($res);
+    error_log(json_decode(trim(end(explode('</script>', $res))), true));
     
-    unlink($cookie);
 }
 
