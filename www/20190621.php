d7442ec05a20043e201ae5c34c5056f5150c2ab7
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-09 09:55:56 +0900

    Update 20190621.php

--- www/20190621.php
+++ www/20190621.php
@@ -25,29 +25,19 @@ function func_20190621($mu_)
     $tmp = explode('</script>', $res);
     $tmp = trim(end($tmp));
     // error_log($tmp);
-    // error_log(mb_detect_encoding($tmp));
     
     $rc = preg_match('/"station": {(.+?)}/s', $tmp, $match);
-    // error_log(print_r($match, true));
     $stations = json_decode('{' . $match[1] . '}', true);
     error_log(print_r($stations, true));
     
     $rc = preg_match('/"train": {(.+?)}/s', $tmp, $match);
-    // error_log(print_r($match, true));
     $trains = json_decode('{' . $match[1] . '}', true);
     error_log(print_r($trains, true));
     
-    /*
-    $json = json_decode($tmp, true);
-    error_log('json_last_error : ' . json_last_error());
-    error_log('json_last_error_msg : ' . json_last_error_msg());
-    */
-    /*
     $url = 'https://traininfo.jr-central.co.jp/shinkansen/var/train_info/train_location_info.json';
     $res = $mu_->get_contents_proxy($url);
     $tmp = explode('</script>', $res);
     $tmp = json_decode(trim(end($tmp)), true);
     error_log(print_r($tmp, true));
-    */
 }
 
