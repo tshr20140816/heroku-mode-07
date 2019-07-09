75c9928f6a74262f21b17aa8ae83342b9a5d2bce
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-09 13:24:23 +0900

    Update 20190621.php

--- www/20190621.php
+++ www/20190621.php
@@ -108,7 +108,7 @@ function func_20190621($mu_)
     $scales->xAxes[] = ['id' => 'x-axis-0',
                         'display' => true,
                         'labels' => $labels,
-                        'ticks' => ['fontColor' => 'n;ack',
+                        'ticks' => ['fontColor' => 'black',
                                    ],
                        ];
     $scales->xAxes[] = ['id' => 'x-axis-1',
