8e72ca537b8dd0af77507df58a92b44367f578d8
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-10 15:59:30 +0900

    Update 20190601.php

--- www/20190601.php
+++ www/20190601.php
@@ -20,31 +20,40 @@ function func_20190601($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
     
-    $url = 'https://www.jtb.co.jp/kokunai_tour/list/1301/?departure=HIJ&capacity=2&godate=20190830&traveldays=2&room=1&transportation=2&samehm=1&toursort=low&&page=1&itemperpage=20';
-    $url = 'https://www.jtb.co.jp/kokunai_tour/list/130302/?departure=HIJ&capacity=2&godate=20190830&traveldays=2&room=1&transportation=2&samehm=1&toursort=low&page=1&itemperpage=20';
-    $url = 'https://www.jtb.co.jp/kokunai_tour/list/130301/?departure=HIJ&capacity=2&godate=20190830&traveldays=2&room=1&transportation=2&samehm=1&toursort=low&page=1&itemperpage=20';
-    $res = $mu_->get_contents($url);
+    $list_item = [];
     
-    // error_log($res);
-    
-    $tmp = explode('<article class="', $res);
-    
-    for ($i = 0; $i < 5; $i++) {
-        // error_log($tmp[$i + 1]);
-        $rc = preg_match('/<h3 class="domtour-tour-list__name"><a .*?href=".+?\?(.+?)".*?>(.+?)<.+?<span class="dom-hotel-price__adult-price"><em>(.+?)</s', $tmp[$i + 1], $match);
-        array_shift($match);
-        error_log(print_r($match, true));
-        
-        // $plan_name = $match[1];
-        
-        $url = 'https://www.jtb.co.jp/kokunai_tour/spookserver?Command=TourShouhinListData&hotelsort=low&page=1&rating=5-4&' . str_replace('&amp;', '&', $match[0]);
+    $urls = [];
+    $urls[] = 'https://www.jtb.co.jp/kokunai_tour/list/1301/?departure=HIJ&capacity=2&godate=20190830&traveldays=2&room=1&transportation=2&samehm=1&toursort=low&&page=1&itemperpage=20';
+    $urls[] = 'https://www.jtb.co.jp/kokunai_tour/list/130302/?departure=HIJ&capacity=2&godate=20190830&traveldays=2&room=1&transportation=2&samehm=1&toursort=low&page=1&itemperpage=20';
+    $urls[] = 'https://www.jtb.co.jp/kokunai_tour/list/130301/?departure=HIJ&capacity=2&godate=20190830&traveldays=2&room=1&transportation=2&samehm=1&toursort=low&page=1&itemperpage=20';
+    foreach ($urls as $url) {
         $res = $mu_->get_contents($url);
-        
-        // error_log($res);
-        $json = json_decode($res);
-        // error_log(print_r($json, true));
-        foreach ($json->tourShouhinList as $item) {
-            error_log($item->shisetsu_name . ' '. $item->min_price);
+
+        $tmp = explode('<article class="', $res);
+
+        for ($i = 0; $i < 5; $i++) {
+            // error_log($tmp[$i + 1]);
+            $rc = preg_match('/<h3 class="domtour-tour-list__name"><a .*?href=".+?\?(.+?)".*?>(.+?)</s', $tmp[$i + 1], $match);
+            array_shift($match);
+            // error_log(print_r($match, true));
+
+            // $plan_name = $match[1];
+            $list_item[] = $match[1];
+            $list_item[] = '';
+
+            $url = 'https://www.jtb.co.jp/kokunai_tour/spookserver?Command=TourShouhinListData&hotelsort=low&page=1&rating=5-4&'
+                . str_replace('&amp;', '&', $match[0]);
+            $res = $mu_->get_contents($url);
+
+            // error_log($res);
+            $json = json_decode($res);
+            // error_log(print_r($json, true));
+            foreach ($json->tourShouhinList as $item) {
+                // error_log($item->shisetsu_name . ' '. $item->min_price);
+                $list_item[] = $item->shisetsu_name . ' '. $item->min_price;
+            }
+            $list_item[] = '';
         }
     }
+    error_log(print_r($list_item, true));
 }
