8f2845b5159a98f298663dd197e4be85edc71523
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-12 10:24:59 +0900

    Update 20190601.php

--- www/20190601.php
+++ www/20190601.php
@@ -22,9 +22,68 @@ function func_20190601($mu_)
     
     $url = 'https://www.train-guide.westjr.co.jp/api/v3/sanyo2_st.json';
     $res = $mu_->get_contents($url, null, true);
-    error_log(print_r(json_decode($res, true), true));
+    // error_log(print_r(json_decode($res, true), true));
     
+    $stations = [];
+    
+    $data = [];
+    $data['station'] = [];
+    $tmp_labels = [];
+    foreach (json_decode($res, true)['stations'] as $station) {
+        $stations[$station['info']['code']] = $station['info']['name'];
+        
+        $tmp_labels[] = '';
+        $tmp_labels[] = $item;
+        $data['station'] = 0;
+        $data['station'] = 0;
+    }
+    array_shift($tmp_labels);
+    $labels = $tmp_labels;
+    array_shift($data['station']);
+    
+    $scales = new stdClass();
+    $scales->xAxes[] = ['id' => 'x-axis-0',
+                        'display' => true,
+                        'labels' => array_reverse($labels),
+                        'ticks' => ['fontColor' => 'black',
+                                   ],
+                       ];
+    
+    $datasets = [];
+    $datasets[] = ['data' => array_reverse($data['station']),
+                   'fill' => false,
+                   'xAxisID' => 'x-axis-0',
+                   'pointRadius' => 0,
+                   'showLine' => false,
+                   'borderColor' => 'rgba(0,0,0,0)',
+                   'backgroundColor' => 'rgba(0,0,0,0)',
+                   'pointStyle' => 'circle',
+                   'pointRadius' => 1,
+                   'pointBackgroundColor' => 'black',
+                   'pointBorderColor' => 'black',
+                   // 'label' => ($bound_ === 1 ? '<上り> ' : '<下り> ') . date('Y/m/d H:i', $dt),
+                  ];
+    
+    $json = ['type' => 'line',
+             'data' => ['labels' => array_reverse($labels),
+                        'datasets' => $datasets,
+                       ],
+             'options' => ['legend' => ['labels' => ['fontColor' => 'black',],],
+                           'animation' => ['duration' => 0,],
+                           'hover' => ['animationDuration' => 0,],
+                           'responsiveAnimationDuration' => 0,
+                           'scales' => $scales,
+                          ],
+            ];
+    
+    $url = 'https://quickchart.io/chart?width=1500&height=210&c=' . urlencode(json_encode($json));
+    $res = $mu_->get_contents($url);
+    
+    header('Content-Type: image/png');
+    echo $res;
+    /*
     $url = 'https://www.train-guide.westjr.co.jp/api/v3/sanyo2.json';
     $res = $mu_->get_contents($url);
     error_log(print_r(json_decode($res, true), true));
+    */
 }
