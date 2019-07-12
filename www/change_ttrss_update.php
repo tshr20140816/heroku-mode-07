0b26bf2fee31d3a1554d95309ad41684234f9403
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-12 14:21:15 +0900

    Update change_ttrss_update.php

--- www/change_ttrss_update.php
+++ www/change_ttrss_update.php
@@ -9,11 +9,35 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$sql = <<< __HEREDOC__
-UPDATE t_env
-   SET value = CASE value WHEN 1 THEN 2 ELSE 1 END
+$sql_update = <<< __HEREDOC__
+UPDATE m_env
+   SET value = CASE value WHEN '1' THEN '2' ELSE '1' END
  WHERE key = 'TTRSS_SELECTED'
 __HEREDOC__;
+
+$sql_select = <<< __HEREDOC__
+SELECT M1.value
+  FROM m_env M1
+ WHERE M1.key = 'URL_TTRSS_' || ( SELECT M2.value
+                                    FROM m_env M2
+                                   WHERE M2.key = 'TTRSS_SELECTED'
+                                )
+__HEREDOC__;
+
+$pdo = $mu->get_pdo();
+$statement = $pdo->prepare($sql_update);
+$rc = $statement->execute();
+
+foreach ($pdo->query($sql_select) as $row) {
+   $url = $row['value'];
+}
+
+$pdo = null;
+
+$rc = apcu_clear_cache();
+
+header('Content-Type: plain/text');
+echo substr($url, 0, 5);
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 exit();
