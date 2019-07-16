f931ab0267a3bf3301f852888fb3eceeea3ae238
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-16 14:46:10 +0900

    Update 20190716.php

--- www/20190716.php
+++ www/20190716.php
@@ -27,7 +27,7 @@ function func_20190716b($mu_)
     
     $options = [CURLOPT_HTTPHEADER => ["Authorization: Bearer ${api_token}",],];
     
-    $url = "https://${host}.loggly.com/apiv2/events/iterate?q=Fatal tag:" . getenv('HEROKU_APP_NAME') . '&from=-24h&until=now&order=asc&size=50';
+    $url = "https://${host}.loggly.com/apiv2/events/iterate?q=Fatal tag:" . getenv('HEROKU_APP_NAME') . '&from=-2d&until=now&order=asc&size=50';
     $res = $mu_->get_contents($url, $options);
     
     error_log($res);
