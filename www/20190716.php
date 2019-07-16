a9d75ee0952b6d75e5e12a0457ca8f99cd388cb6
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-16 11:39:12 +0900

    Update 20190716.php

--- www/20190716.php
+++ www/20190716.php
@@ -32,7 +32,7 @@ function func_20190716b($mu_)
     ];
     
     $url = "https://${host}.loggly.com/apiv2/search?q=*&from=-30m&until=now";
-    $res = $mu_->get_contents($url);
+    $res = $mu_->get_contents($url, $options);
     
     error_log($res);
 }
