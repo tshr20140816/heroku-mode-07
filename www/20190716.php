2a34d40fbe4b669696a4c329b18fb0669607c003
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-16 13:08:07 +0900

    Update 20190716.php

--- www/20190716.php
+++ www/20190716.php
@@ -34,8 +34,9 @@ function func_20190716b($mu_)
     
     foreach (json_decode($res, true) as $items) {
         foreach ($items as $item) {
-            error_log(print_r($item, true));
-            break;
+            if (in_array('HEROKU_APP_NAME', $item['tags'], true)) {
+                error_log(print_r($item, true));
+            }
         }
     }
     
