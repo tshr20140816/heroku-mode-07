334fcbb53076cc645c125116fb98e6b7d319c143
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-18 09:15:54 +0900

    Update daily020.php

--- www/daily020.php
+++ www/daily020.php
@@ -23,7 +23,7 @@ $file_name_blog = tempnam("/tmp", 'blog_' .  md5(microtime(true)));
 @unlink($file_name_blog);
 
 exec('cd /app && composer update > /dev/null 2>&1 &');
-exec('curl --head ' . $mu->get_env('URL_TTRSS_1') . ' > /dev/null 2>&1 &');
+// exec('curl --head ' . $mu->get_env('URL_TTRSS_1') . ' > /dev/null 2>&1 &');
 
 // quota
 get_quota($mu, $file_name_blog);
