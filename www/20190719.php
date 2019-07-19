273b982b5427f36d49d54a6e5167d4399226a38f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-19 13:02:41 +0900

    Update 20190719.php

--- www/20190719.php
+++ www/20190719.php
@@ -1,16 +1,25 @@
 <?php
+
 include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
 $pid = getmypid();
 $requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
 $mu = new MyUtils();
+
 $rc = func_20190719($mu);
-$time_finish = microtime(true);
+
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 exit();
+
 function func_20190719($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
+    $url = 'http://www1.river.go.jp/cgi-bin/DspDamData.exe?ID=1368080700010&KIND=3&PAGE=0';
+    $res = $mu_->get_contents($url);
+    
+    error_log($res);
 }
