f16e9436550ea3b29445e9746fd75888aafe4c68
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-22 13:00:35 +0900

    Update 20190601.php

--- www/20190601.php
+++ www/20190601.php
@@ -9,12 +9,21 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$rc = func_20190601b($mu);
+// $rc = func_20190601b($mu);
+$rc = func_20190601c($mu);
 
 $time_finish = microtime(true);
 
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
 exit();
+
+function func_20190601c($mu_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+
+    $url = 'https://www.train-guide.westjr.co.jp/api/v3/sanyo2_st.json';
+    $res = $mu_->get_contents($url, null, true);
+}
 
 function func_20190601b($mu_)
 {
