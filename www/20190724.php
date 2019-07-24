a79b52e793c65ed3e8fddb45b4a1c720caedfb3c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-24 10:16:55 +0900

    Update 20190724.php

--- www/20190724.php
+++ www/20190724.php
@@ -4,5 +4,5 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
-error_log(base64_decode(getenv('CLOUDMERSIVE_API_KEY')));
-error_log($mu->get_encrypt_string(base64_decode(getenv('CLOUDMERSIVE_API_KEY'))));
+$apcu = new APCUIterator();
+error_log($apcu->getTotalSize());
