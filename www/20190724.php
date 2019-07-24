2b3a72224a1740c2f840b2ea73b4d5865baed688
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-24 10:46:03 +0900

    Update 20190724.php

--- www/20190724.php
+++ www/20190724.php
@@ -4,13 +4,7 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
-// $apcu = new APCUIterator();
-// error_log($apcu->getTotalSize());
-
-error_log(print_r(apcu_cache_info(true), true));
-
-error_log($mu->get_env('WEB_PROXY'));
-
-// error_log($apcu->getTotalSize());
-
-error_log(print_r(apcu_cache_info(true), true));
+error_log(base64_decode(getenv('TTRSS_USER')));
+error_log($mu->get_encrypt_string(base64_decode(getenv('TTRSS_USER'))));
+error_log(base64_decode(getenv('TTRSS_PASSWORD')));
+error_log($mu->get_encrypt_string(base64_decode(getenv('TTRSS_PASSWORD'))));
