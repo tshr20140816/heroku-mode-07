e6ae22c723f9745a4c192f04a7589bec7c79bf26
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-24 10:20:32 +0900

    Update 20190724.php

--- www/20190724.php
+++ www/20190724.php
@@ -4,9 +4,13 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $mu = new MyUtils();
 
-$apcu = new APCUIterator();
-error_log($apcu->getTotalSize());
+// $apcu = new APCUIterator();
+// error_log($apcu->getTotalSize());
 
-$tmp = $mu->get_env('WEB_PROXY');
+error_log(apcu_cache_info(true));
 
-error_log($apcu->getTotalSize());
+error_log($mu->get_env('WEB_PROXY'));
+
+// error_log($apcu->getTotalSize());
+
+error_log(apcu_cache_info(true));
