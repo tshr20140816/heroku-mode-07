f2cf3551520d9948cbb099f21f51e49a9786e824
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-25 13:19:01 +0900

    Update and rename 20190601.php to train_sanyo2.php

--- www/train_sanyo2.php
+++ www/train_sanyo2.php
@@ -9,85 +9,85 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$rc = func_20190601f($mu);
+$rc = get_train_sanyo2($mu);
 
 $time_finish = microtime(true);
 
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
 exit();
 
-function func_20190601f($mu_) {
+function get_train_sanyo2($mu_) {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
     $url = 'https://www.train-guide.westjr.co.jp/api/v3/sanyo2_st.json';
     $sanyo2_st = $mu_->get_contents($url, null, true);
-    
+
     $url = 'https://www.train-guide.westjr.co.jp/api/v3/sanyo2.json';
     $sanyo2 = $mu_->get_contents($url);
-    
+
     error_log($log_prefix . print_r(json_decode($sanyo2_st, true), true));
     error_log($log_prefix . print_r(json_decode($sanyo2, true), true));
 
     $res1 = func_20190601e($mu_, $sanyo2_st, $sanyo2, '1');
     $res2 = func_20190601e($mu_, $sanyo2_st, $sanyo2, '0');
-    
+
     $im1 = imagecreatefromstring($res1);
     $x = imagesx($im1);
     $y1 = imagesy($im1);
     imagedestroy($im1);
-    
+
     $im1 = imagecreatefromstring($res2);
     // $x = imagesx($im1);
     $y2 = imagesy($im1);
     imagedestroy($im1);
-    
+
     $im1 = imagecreatetruecolor($x, $y1 + $y2);
     imagefill($im1, 0, 0, imagecolorallocate($im1, 255, 255, 255));
-    
+
     $im2 = imagecreatefromstring($res1);
     imagecopy($im1, $im2, 0, 0, 0, 0, $x, $y1);
     imagedestroy($im2);
-    
+
     $im2 = imagecreatefromstring($res2);
     imagecopy($im1, $im2, 0, $y1, 0, 0, $x, $y2);
     imagedestroy($im2);
-    
+
     $im2 = imagerotate($im1, 270, imagecolorallocate($im1, 0, 0, 0));
     imagedestroy($im1);
-    
+
     $x = imagesx($im2);
     $y = imagesy($im2);
-    
+
     $im1 = imagecreatetruecolor($x, $y / 3);
     imagefill($im1, 0, 0, imagecolorallocate($im1, 255, 255, 255));
-    
+
     imagecopy($im1, $im2, 0, 0, 0, 0, $x, $y / 3);
-    
+
     $file = tempnam("/tmp", md5(microtime(true)));
     imagepng($im1, $file, 9);
     imagedestroy($im1);
     $res1 = file_get_contents($file);
     unlink($file);
-    
+
     $im1 = imagecreatetruecolor($x, $y / 3 * 2);
     imagefill($im1, 0, 0, imagecolorallocate($im1, 255, 255, 255));
-    
+
     imagecopy($im1, $im2, 0, 0, 0, $y / 3, $x, $y / 3 * 2);
     imagedestroy($im2);
-    
+
     $file = tempnam("/tmp", md5(microtime(true)));
     imagepng($im1, $file, 9);
     imagedestroy($im1);
     $res2 = file_get_contents($file);
     unlink($file);
-    
+
     echo '<html><body><font size="7">' . date('H:i:s', strtotime(json_decode($sanyo2, true)['update']) + 60 * 60 * 9)
         . '</font><br><img width="100%" src="data:image/png;base64,' . base64_encode($res1)
         . '"><br><a name="p1" /><img width="100%" src="data:image/png;base64,' . base64_encode($res2)
         . '"></body></html>';
 }
 
-function func_20190601e($mu_, $sanyo2_st_, $sanyo2_, $direction_ = '0') // $direction_ : '0' nobori / '1' kudari
+function get_train_sanyo2_image($mu_, $sanyo2_st_, $sanyo2_, $direction_ = '0') // $direction_ : '0' nobori / '1' kudari
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
@@ -98,19 +98,19 @@ function func_20190601e($mu_, $sanyo2_st_, $sanyo2_, $direction_ = '0') // $dire
         $stations[$station['info']['code']]['name'] = $station['info']['name'];
         $stations[$station['info']['code']]['index'] = $index;
         $index += 2;
-        
+
         $labels['station'][] = '';
         $labels['station'][] = $station['info']['name'];
     }
     array_shift($labels['station']);
-    
+
     $labels['real'] = [];
     $labels['dest'] = [];
     for ($i = 0; $i < count($labels['station']); $i++) {
         $labels['real'][] = (string)$i;
         $labels['dest'][] = '';
     }
-    
+
     error_log($log_prefix . print_r($labels, true));
     error_log($log_prefix . print_r($stations, true));
 
@@ -184,9 +184,9 @@ function func_20190601e($mu_, $sanyo2_st_, $sanyo2_, $direction_ = '0') // $dire
     }
     error_log($log_prefix . print_r($data, true));
     error_log($log_prefix . print_r($labels, true));
-    
+
     $pointRotation = $direction_ === '0' ? 270 : 90;
-    
+
     $datasets[] = ['data' => $data['ontime'],
                    'fill' => false,
                    'showLine' => false,
@@ -199,7 +199,7 @@ function func_20190601e($mu_, $sanyo2_st_, $sanyo2_, $direction_ = '0') // $dire
                    'pointBorderColor' => 'red',
                    'pointBorderWidth' => 2,
                   ];
-    
+
     $datasets[] = ['data' => $data['delay'],
                    'fill' => false,
                    'showLine' => false,
@@ -212,7 +212,7 @@ function func_20190601e($mu_, $sanyo2_st_, $sanyo2_, $direction_ = '0') // $dire
                    'pointBorderColor' => 'cyan',
                    'pointBorderWidth' => 3,
                   ];
-    
+
     $datasets[] = ['data' => $data['ontime_etc'],
                    'fill' => false,
                    'showLine' => false,
@@ -225,7 +225,7 @@ function func_20190601e($mu_, $sanyo2_st_, $sanyo2_, $direction_ = '0') // $dire
                    'pointBorderColor' => 'red',
                    'pointBorderWidth' => 2,
                   ];
-    
+
     $datasets[] = ['data' => $data['delay_etc'],
                    'fill' => false,
                    'showLine' => false,
@@ -238,11 +238,11 @@ function func_20190601e($mu_, $sanyo2_st_, $sanyo2_, $direction_ = '0') // $dire
                    'pointBorderColor' => 'cyan',
                    'pointBorderWidth' => 3,
                   ];
-    
+
     $tmp = new stdClass();
     $tmp->x = $direction_ === '0' ? '糸崎' : '岩国';
     $tmp->y = 0;
-    
+
     $datasets[] = ['data' => [$tmp, ],
                    'fill' => false,
                    'showLine' => false,
@@ -254,7 +254,7 @@ function func_20190601e($mu_, $sanyo2_st_, $sanyo2_, $direction_ = '0') // $dire
                    'pointBorderColor' => 'black',
                    // 'pointBorderWidth' => 3,
                   ];
-    
+
     $scales = new stdClass();
     $scales->xAxes[] = ['id' => 'x-axis-0',
                         'display' => false,
@@ -284,7 +284,7 @@ function func_20190601e($mu_, $sanyo2_st_, $sanyo2_, $direction_ = '0') // $dire
                                     'min' => 0,
                                    ],
                        ];
-    
+
     $annotations = [];
     for ($i = 0; $i < count($labels['dest']); $i++) {
         if ($labels['dest'][$i] !== '') {
@@ -324,7 +324,7 @@ function func_20190601e($mu_, $sanyo2_st_, $sanyo2_, $direction_ = '0') // $dire
                       'borderColor' => 'rgba(255,100,100,200)',
                       'borderWidth' => 3,
                      ];
-    
+
     $json = ['type' => 'line',
              'data' => ['labels' => $labels['real'],
                         'datasets' => $datasets,
@@ -345,23 +345,6 @@ function func_20190601e($mu_, $sanyo2_st_, $sanyo2_, $direction_ = '0') // $dire
     $url = "https://quickchart.io/chart?width=1500&height=${height}&c=" . urlencode(json_encode($json));
     $res = $mu_->get_contents($url);
     error_log($log_prefix . 'URL length : ' . number_format(strlen($url)));
-    
-    /*
-    $im1 = imagecreatefromstring($res);
-    error_log($log_prefix . imagesx($im1) . ' ' . imagesy($im1));
-    $im2 = imagecreatetruecolor(imagesx($im1) / 3, imagesy($im1) / 3);
-    // imagealphablending($im2, false);
-    // imagesavealpha($im2, true);
-    imagefill($im2, 0, 0, imagecolorallocate($im1, 255, 255, 255));
-    imagecopyresampled($im2, $im1, 0, 0, 0, 0, imagesx($im1) / 3, imagesy($im1) / 3, imagesx($im1), imagesy($im1));
-    imagedestroy($im1);
-    $file = tempnam('/tmp', 'png_' . md5(microtime(true)));
-    imagepng($im2, $file, 9);
-    imagedestroy($im2);
-    error_log($log_prefix . 'file size : ' . number_format(filesize($file)));
-    $res = file_get_contents($file);
-    unlink($file);
-    */
-    
+
     return $res;
 }
