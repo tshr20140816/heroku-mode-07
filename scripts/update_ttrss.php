a2f55c21603287b8ee365d0da21f216bd078e09a
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-26 11:49:08 +0900

    Update update_ttrss.php

--- scripts/update_ttrss.php
+++ scripts/update_ttrss.php
@@ -53,6 +53,9 @@ __HEREDOC__;
     $livedoor_id = $mu_->get_env('LIVEDOOR_ID', true);
     $urls[] = "http://blog.livedoor.jp/${livedoor_id}/atom.xml";
 
+    $hatena_blog_id = $mu_->get_env('HATENA_BLOG_ID', true);
+    $urls[] = "https://${hatena_blog_id}/feed";
+
     $fc2_fqdn = $mu_->get_env('FC2_FTP_SERVER', true);
     if ((int)date('H', strtotime('+9 hours')) < 19) {
         $urls[] = "https://${fc2_fqdn}/" . getenv('FC2_RSS_01') . '.xml';
@@ -61,6 +64,7 @@ __HEREDOC__;
         $urls[] = "https://${fc2_fqdn}/" . getenv('FC2_RSS_04') . '.xml';
         $urls[] = "https://${fc2_fqdn}/" . getenv('FC2_RSS_05') . '.xml';
     }
+    error_log($log_prefix . print_r($urls, true));
 
     $json = '{"sid":"' . $session_id . '","op":"getFeeds","cat_id":-3}';
     $res = $mu_->get_contents($url, $options + [CURLOPT_POSTFIELDS => $json,]);
