6482c2d4d609b20981847171b9bb3d94286d86fd
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-26 09:22:38 +0900

    Update 20190726.php

--- www/20190726.php
+++ www/20190726.php
@@ -17,4 +17,21 @@ exit();
 function func_20190726($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+    
+    $hatena_id = $mu_->get_env('HATENA_ID', true);
+    $hatena_blog_id = $mu_->get_env('HATENA_BLOG_ID', true);
+    $hatena_api_key = $mu_->get_env('HATENA_API_KEY', true);
+    
+    $url = "https://blog.hatena.ne.jp/${hatena_id}/${hatena_blog_id}/atom/entry";
+    
+    $options = [
+        CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+        CURLOPT_USERPWD => "${hatena_id}:${hatena_api_key}",
+        CURLOPT_HEADER => true,
+        CURLOPT_HTTPHEADER => ['Expect:',],
+    ];
+    
+    $res = $mu_->get_contents($url, $options);
+    
+    error_log($res);
 }
