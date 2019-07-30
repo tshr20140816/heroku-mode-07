07bf50c953b47ab054e4773237ecf9a650ae5313
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-30 14:58:09 +0900

    Update 20190730.js

--- scripts/20190730.js
+++ scripts/20190730.js
@@ -2,24 +2,20 @@ const ChartjsNode = require('chartjs-node');
 
 var chartNode = new ChartjsNode(process.argv[2], process.argv[3]);
 
+var chartJsOptions = {"type":"line","data":{"datasets":[{"data":[1,2,3,3,2,1]}]},"options":{}};
 /*
-var chartJsOptions = {type: 'line',
-                      data: {datasets: [{data: [1, 2]}]},
-                      options: {}
-                     };
-*/
-
 var buffer = Buffer.from(process.argv[4], 'base64');
 var chartJsOptions = buffer.toString('ascii');
 console.log(process.argv[4]);
 console.error(process.argv[4]);
+*/
 console.log(chartJsOptions);
 console.error(chartJsOptions);
 
 return chartNode.drawChart(chartJsOptions)
 .then(() => {
-    console.log(process.argv[4]);
-    console.error(process.argv[4]);
+    // console.log(process.argv[4]);
+    // console.error(process.argv[4]);
     console.log(chartJsOptions);
     console.error(chartJsOptions);
     return chartNode.getImageBuffer('image/png');
