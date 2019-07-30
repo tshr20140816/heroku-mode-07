8e06756d62efecf6d2e3ed04e26a5b2388d405d0
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-30 16:28:50 +0900

    Update 20190730.js

--- scripts/20190730.js
+++ scripts/20190730.js
@@ -7,7 +7,7 @@ var util = require('util');
 // var chartJsOptions = {"type":"line","data":{"datasets":[{"data":[1,2,3,3,2,1]}]},"options":{}};
 var chartJsOptions = {"type":"line","data":{"datasets":[{"data":[1,2]}]},"options":{"legend":{"display":false}}};
 
-var buffer = Buffer.from(process.argv[4], 'base64');
+// var buffer = Buffer.from(process.argv[4], 'base64');
 // var chartJsOptions = JSON.stringify(JSON.parse(buffer.toString()));
 
 console.error(process.argv[4]);
