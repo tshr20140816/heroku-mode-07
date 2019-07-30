fc958e2ef83c70bd0fda301585754bcb36f5f221
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-31 08:58:00 +0900

    Update 20190730.js

--- scripts/20190730.js
+++ scripts/20190730.js
@@ -6,22 +6,17 @@ chartNode.on('beforeDraw', function (Chartjs) {
     Chartjs.defaults.global.defaultFontFamily = 'IPAexGothic';
 });
 
-// var util = require('util');
+var util = require('util');
 
-/*
-var buffer = Buffer.from(process.argv[4], 'base64');
-var chartJsOptions = JSON.parse(buffer.toString('utf-8'));
-*/
 var chartJsOptions = JSON.parse(Buffer.from(process.argv[4], 'base64').toString('utf-8'));
 
 // console.error(process.argv[4]);
 
 console.error(chartJsOptions);
-// console.error(util.inspect(chartJsOptions, false, null));
+console.error(util.inspect(chartJsOptions, false, null));
 
 return chartNode.drawChart(chartJsOptions)
 .then(() => {
-    // console.error(util.inspect(chartJsOptions, false, null));
     return chartNode.getImageBuffer('image/png');
 })
 .then(buffer => {
