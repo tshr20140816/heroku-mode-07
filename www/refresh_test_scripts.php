42b72bf215eb9fdab853a295e7e901177c4077ae
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-31 09:38:13 +0900

    Create refresh_test_scripts.php

---
+++ www/refresh_test_scripts.php
@@ -0,0 +1,16 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+$url = 'https://github.com/tshr20140816/heroku-mode-07/tree/master/www';
+
+$res = $mu->get_contents_proxy($url);
+
+error_log($res);
