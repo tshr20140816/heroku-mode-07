4f6eda198fe463ec6326347fbf7d2eeab1a7d54d
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-31 13:05:48 +0900

    Update chartjs_node.js

--- scripts/chartjs_node.js
+++ scripts/chartjs_node.js
@@ -6,7 +6,7 @@ var chartNode = new ChartjsNode(process.argv[2], process.argv[3]);
 chartNode.on('beforeDraw', function (Chartjs) {
     Chartjs.defaults.global.defaultFontFamily = 'IPAexGothic';
     Chartjs.pluginService.register(ChartjsAnnotation);
-    console.error(Chartjs.plugins)
+    // console.error(Chartjs.plugins)
 });
 
 var util = require('util');
@@ -14,13 +14,12 @@ var util = require('util');
 var chartJsOptions = JSON.parse(Buffer.from(process.argv[4], 'base64').toString('utf-8'));
 
 // console.error(process.argv[4]);
-
 // console.error(chartJsOptions);
-console.error(util.inspect(chartJsOptions, false, null));
+// console.error(util.inspect(chartJsOptions, false, null));
 
 return chartNode.drawChart(chartJsOptions)
 .then(streamResult => {
-    console.error(chartJsOptions);
+    // console.error(chartJsOptions);
     return chartNode.writeImageToFile('image/png', process.argv[5]);
 });
 
