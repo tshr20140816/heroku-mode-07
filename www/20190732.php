0ec8c8a8b102b25ee28b3e80f6f3960b40d3c4ed
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-05 10:54:08 +0900

    Update 20190732.php

--- www/20190732.php
+++ www/20190732.php
@@ -23,7 +23,7 @@ function func_20190732g($mu_, $file_name_rss_items_)
     $results = [];
     for ($i = 3; $i < 11; $i++) {
         $url = 'http://npb.jp/games/2019/schedule_' . str_pad($i, 2, '0', STR_PAD_LEFT) . '_detail.html';
-        $res = $mu_->get_contents($url);
+        $res = $mu_->get_contents($url, null, true);
 
         $tmp = explode('<tr id="date', $res);
         foreach ($tmp as $item) {
