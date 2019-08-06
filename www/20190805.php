9e0cafd8b4b940029616486a5a52d6cb120a0fb7
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-06 12:56:04 +0900

    Update 20190805.php

--- www/20190805.php
+++ www/20190805.php
@@ -27,14 +27,18 @@ function func_20190805($mu_)
     exec($cmd, $res);
     error_log($log_prefix . print_r($res, true));
     
-    $res = hash_file('sha256', $file_name);
-    // error_log($log_prefix . 'sha256 start : ' . $res);
+    error_log($log_prefix . 'sha256 start : ' . hash_file('sha256', $file_name));
+    error_log($log_prefix . 'filesize start : ' . filesize($file_name));
     
-    // copy($file_name, $file_name . 'a');
-    // copy($file_name, $file_name . 'b');
-    
-    // func_20190805c($mu_, file_get_contents($file_name . 'a'), $file_name . 'c');
+    clearstatcache();
     func_20190805b($mu_, $file_name);
+    clearstatcache();
+    func_20190805c($mu_, $file_name);
+    clearstatcache();
+    
+    $res = hash_file('sha256', $file_name);
+    error_log($log_prefix . 'sha256 finish : ' . hash_file('sha256', $file_name));
+    error_log($log_prefix . 'filesize finish : ' . filesize($file_name));
 }
 
 function func_20190805b($mu_, $file_name_)
@@ -59,7 +63,6 @@ function func_20190805b($mu_, $file_name_)
     
     $line = 'openssl ' . $method . ' -e -base64 -A -iv ' . $iv . ' -pass pass:' . $password . ' -in ' . $file_name_ . '.bz2 -out ' . $file_name_;
     error_log($log_prefix . $line);
-    
     $res = null;
     exec($line, $res);
     error_log($log_prefix . print_r($res, true));
@@ -69,37 +72,25 @@ function func_20190805b($mu_, $file_name_)
     error_log($log_prefix . 'hash : ' . hash_file('sha256', $file_name_));
 }
 
-function func_20190805c($mu_, $data_, $file_name_)
+function func_20190805c($mu_, $file_name_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
-    
-    @unlink($file_name_ . '.bz2');
-    @unlink($file_name_ . '.enc');
-    
-    $base_name = pathinfo($file_name_)['basename'];
     
     $user_hidrive = $mu_->get_env('HIDRIVE_USER', true);
     $password_hidrive = $mu_->get_env('HIDRIVE_PASSWORD', true);
     
-    $file_name = tempnam("/tmp", 'bz_' .  md5(microtime(true)));
-    $rc = file_put_contents($file_name, $data_);
-    $data_ = null;
-    $res = null;
-    exec('bzip2 -v ' . $file_name, $res);
-    error_log($log_prefix . print_r($res, true));
-    $res = file_get_contents($file_name . '.bz2');
-    unlink($file_name . '.bz2');
-    
     $method = 'aes-256-cbc';
     $password = base64_encode($user_hidrive) . base64_encode($password_hidrive);
-    $iv = substr(sha1('hoge'), 0, openssl_cipher_iv_length($method));
-    $res = openssl_encrypt($res, $method, $password, OPENSSL_RAW_DATA, $iv);
+    $iv = substr(sha1($file_name_), 0, openssl_cipher_iv_length($method));
     
-    $res = base64_encode($res);
-    error_log($log_prefix . substr($res, 0, 200));
-    file_put_contents($file_name_, $res);
+    $line = 'openssl ' . $method . ' -d -base64 -A -iv ' . $iv . ' -pass pass:' . $password . ' -in ' . $file_name_ . '-out ' . $file_name_ . '.bz2';
+    error_log($log_prefix . $line);
     $res = null;
+    exec($line, $res);
+    error_log($log_prefix . print_r($res, true));
+    unlink($file_name_);
     
-    error_log($log_prefix . 'size : ' . number_format(filesize($file_name_)));
-    error_log($log_prefix . 'hash : ' . hash_file('sha256', $file_name_));
+    $res = null;
+    exec('bunzip2 -v ' . $file_name_ . '.bz2', $res);
+    error_log($log_prefix . print_r($res, true));
 }
