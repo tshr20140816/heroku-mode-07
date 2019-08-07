a5eca06f60004fd96bcf457eba6bf324d0899893
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-07 15:47:39 +0900

    Update 20190732.php

--- www/20190732.php
+++ www/20190732.php
@@ -9,12 +9,148 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-func_20190732g($mu, '/tmp/dummy20190732a');
+func_20190732h($mu, '/tmp/dummy20190732a');
 
 $time_finish = microtime(true);
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 exit();
+
+function func_20190732h($mu_, $file_name_rss_items_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+
+    for ($i = 0; $i < (int)date('t'); $i++) {
+        $labels[] = $i + 1;
+        $tmp = new stdClass();
+        $tmp->x = $i + 1;
+        $tmp->y = ((int)date('t') - $i) * 24;
+        $data1[] = $tmp;
+    }
+
+    $datasets = [];
+    $datasets[] = ['data' => $data1,
+                   'fill' => false,
+                   'pointStyle' => 'line',
+                   'backgroundColor' => 'black',
+                   'borderColor' => 'black',
+                   'borderWidth' => 1,
+                   'pointRadius' => 0,
+                   'label' => 'max',
+                  ];
+
+    $list = [['target' => 'toodledo',
+              'color' => 'green',
+              'planColor' => 'red',
+             ],
+             ['target' => 'ttrss',
+              'color' => 'deepskyblue',
+              'planColor' => 'orange',
+             ],
+             ['target' => 'redmine',
+              'color' => 'blue',
+              'planColor' => 'yellow',
+             ],
+            ];
+
+    $sql = <<< __HEREDOC__
+SELECT T1.value
+  FROM t_data_log T1
+ WHERE T1.key = :b_key
+__HEREDOC__;
+
+    $pdo = $mu_->get_pdo();
+    $statement = $pdo->prepare($sql);
+
+    foreach ($list as $one_data) {
+        error_log(print_r($one_data, true));
+        $statement->execute([':b_key' => strtoupper($one_data['target'])]);
+        $result = $statement->fetchAll();
+        $quotas = json_decode($result[0]['value'], true);
+        error_log(print_r($quotas, true));
+
+        $data2 = [];
+        foreach ($quotas as $key => $value) {
+            $tmp = new stdClass();
+            $tmp->x = (int)substr($key, -2) - 1;
+            $tmp->y = (int)($value / 3600);
+            $data2[] = $tmp;
+        }
+
+        if (count($data2) < 3) {
+            return 0;
+        }
+        if ($data2[0]->x == 0) {
+            array_shift($data2);
+            $tmp = new stdClass();
+            $tmp->x = 1;
+            $tmp->y = 550;
+            $data2[0] = $tmp;
+        }
+
+        $datasets[] = ['data' => $data2,
+                       'fill' => false,
+                       'pointStyle' => 'circle',
+                       'backgroundColor' => $one_data['color'],
+                       'borderColor' => $one_data['color'],
+                       'borderWidth' => 3,
+                       'pointRadius' => 4,
+                       'pointBorderWidth' => 0,
+                       'label' => $one_data['target'],
+                      ];
+
+        $data3 = [];
+        $tmp = new stdClass();
+        $tmp->x = 1;
+        $tmp->y = 550;
+        $data3[] = $tmp;
+        $tmp = new stdClass();
+        $tmp->x = (int)date('t');
+        $tmp->y = 550 - (int)((550 - end($data2)->y) / end($data2)->x + 1) * (int)date('t');
+        $data3[] = $tmp;
+
+        $datasets[] = ['data' => $data3,
+                       'fill' => false,
+                       'backgroundColor' => $one_data['planColor'],
+                       'borderWidth' => 3,
+                       'borderColor' => $one_data['planColor'],
+                       'pointRadius' => 0,
+                       'label' => $one_data['target'] . ' plan',
+                      ];
+    }
+
+    $pdo = null;
+
+    $chart_data = ['type' => 'line',
+                   'data' => ['labels' => $labels,
+                              'datasets' => $datasets,
+                             ],
+                   'options' => ['legend' => ['display' => true,
+                                              'labels' => ['boxWidth' => 10,
+                                                          ],
+                                             ],
+                                 'animation' => ['duration' => 0,
+                                                ],
+                                 'hover' => ['animationDuration' => 0,
+                                            ],
+                                 'responsiveAnimationDuration' => 0,
+                                 'annotation' => ['annotations' => [['type' => 'line',
+                                                                     'mode' => 'vertical',
+                                                                     'scaleID' => 'x-axis-0',
+                                                                     'value' => count($datasets[1]['data']),
+                                                                    ],
+                                                                   ],
+                                                 ],
+                                ],
+                  ];
+    $file = tempnam('/tmp', 'chartjs_' . md5(microtime(true)));
+    exec('node ../scripts/chartjs_node.js 900 480 ' . base64_encode(json_encode($json)) . ' ' . $file);
+    $res = file_get_contents($file);
+    unlink($file);
+    
+    header('Content-Type: image/png');
+    echo $res;
+}
 
 function func_20190732g($mu_, $file_name_rss_items_)
 {
