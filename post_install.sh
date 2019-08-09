672301c81b3710018b311f21ba6d467913ce4548
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-09 09:42:21 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,7 +4,7 @@ set -x
 
 date
 
-curl -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_000 > /dev/null 2>&1 &
+curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_000 > /dev/null 2>&1 &
 
 grep -c -e processor /proc/cpuinfo
 cat /proc/cpuinfo | head -n $(($(cat /proc/cpuinfo | wc -l) / $(grep -c -e processor /proc/cpuinfo)))
@@ -91,10 +91,10 @@ ls -lang .fonts/
 chmod 755 ./start_web.sh
 chmod 755 ./bin/unrar
 
-curl -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_100 > /dev/null 2>&1
+curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_100 > /dev/null 2>&1
 
 wait
 
-curl -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_200 > /dev/null 2>&1
+curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_200 > /dev/null 2>&1
 
 date
