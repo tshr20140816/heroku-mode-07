8ba132a260fda6426a36794fc006efd98a89f62c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-23 15:06:31 +0900

    Update check_train.php

--- www/check_train.php
+++ www/check_train.php
@@ -63,7 +63,7 @@ function check_train($mu_)
     }
 
     $options = [
-        CURLOPT_ENCODING => 'gzip, deflate, br',
+        CURLOPT_ENCODING => 'gzip, deflate',
         CURLOPT_HTTPHEADER => [
             'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
