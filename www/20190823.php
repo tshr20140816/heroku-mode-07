3a0b820c552f0e8e39639f1b7076412b817ecf27
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-23 13:19:50 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -8,6 +8,7 @@ $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
+
 func_20190823($mu);
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
@@ -24,14 +25,14 @@ function func_20190823($mu_)
     
     $url = "https://webdav.hidrive.strato.com/users/${user_hidrive}/${base_name}";
         
-    $line = 'curl -v -X DELETE -u ' . "${user_opendrive}:${password_opendrive} " . $url;
+    $line = 'curl -v -X DELETE -u ' . "${user_hidrive}:${password_hidrive} " . $url;
     error_log($log_prefix . $line);
     $res = null;
     exec($line, $res);
     error_log($log_prefix . print_r($res, true));
     $res = null;
     
-    $line = 'curl -v -X PUT -T ' . "/tmp/${base_name}" . ' -u ' . "${user_opendrive}:${password_opendrive} " . $url;
+    $line = 'curl -v -X PUT -T ' . "/tmp/${base_name}" . ' -u ' . "${user_hidrive}:${password_hidrive} " . $url;
     error_log($log_prefix . $line);
     $res = null;
     exec($line, $res);
