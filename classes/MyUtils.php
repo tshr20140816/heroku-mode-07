1a58f8376685d5462032e5433274e8d5fce9d9ea
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-24 09:44:05 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -1727,7 +1727,7 @@ megaput -u {$user_mega} -p {$password_mega} --path /Root/{$base_name} {$file_nam
 __HEREDOC__;
         
         file_put_contents('/tmp/jobs.txt', $jobs);
-        $line = 'cat /tmp/jobs.txt | parallel -j6';
+        $line = 'time cat /tmp/jobs.txt | parallel -j6';
         $res = null;
         error_log($log_prefix . $line);
         exec($line, $res);
