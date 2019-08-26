3cd16a201768d797c6678b1e77d324cb05fe6d57
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-26 11:43:01 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -29,7 +29,7 @@ function func_20190823c($mu_)
         $docid = $item->DOCID;
         $url = "https://apidocs.zoho.com/files/v1/content/${docid}?authtoken=${authtoken_zoho}&scope=docsapi";
         $file_name = tempnam('/tmp', 'curl_' .  md5(microtime(true)));
-        $jobs[$file_name] = "curl -D ${file_name} -o /dev/null ${url}";
+        $jobs[$file_name] = "'curl -D ${file_name} -o /dev/null ${url}'";
     }
     
     $jobs = array_chunk($jobs, 3, true)[0];
