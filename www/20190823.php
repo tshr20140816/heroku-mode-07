e9d9b9cf56942c4979bc52c1e87fc78562c5abfa
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-26 12:31:09 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -38,7 +38,7 @@ function func_20190823c($mu_)
     
     file_put_contents('/tmp/jobs.txt', implode("\n", $jobs));
     
-    $line = "cat /tmp/jobs.txt | xargs -L 1 -P 2 -I{} bash -c {}";
+    $line = "cat /tmp/jobs.txt | xargs -L 1 -P 4 -I{} bash -c {}";
     $res = null;
     error_log($log_prefix . $line);
     $time_start = microtime(true);
