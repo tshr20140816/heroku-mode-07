dfe906fde6d27f62ae9428f56c60666e411b904e
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-27 12:42:11 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -540,7 +540,7 @@ __HEREDOC__;
         }
     }
 
-    public function post_blog_hatena($title_, $description_ = null)
+    public function post_blog_hatena($title_, $description_ = null, $category_ = null)
     {
         $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
@@ -556,12 +556,19 @@ __HEREDOC__;
 <?xml version="1.0" encoding="utf-8"?>
 <entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app">
   <title>__TITLE__</title>
+  __CATEGORY__
   <content type="text/plain">__CONTENT__</content>
 </entry>
 __HEREDOC__;
 
         $xml = str_replace('__TITLE__', date('Y/m/d H:i:s', strtotime('+9 hours')) . " ${title_}", $xml);
         $xml = str_replace('__CONTENT__', htmlspecialchars(nl2br($description_)), $xml);
+
+        if ($category_ === null) {
+            $xml = str_replace('__CATEGORY__', '', $xml);
+        } else {
+            $xml = str_replace('__CATEGORY__', '<category term="' . $category_ . '" />', $xml);
+        }
 
         $url = "https://blog.hatena.ne.jp/${hatena_id}/${hatena_blog_id}/atom/entry";
 
