944253f3df3118827890eb739623a700d114533f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-28 09:58:53 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -40,6 +40,8 @@ __HEREDOC__;
         $docids = unserialize(bzdecompress(base64_decode($result[0]['value'])));
     }
     $result = null;
+    
+    $pdo = null;
 
     $authtoken_zoho = $mu_->get_env('ZOHO_AUTHTOKEN', true);
     
@@ -63,24 +65,65 @@ __HEREDOC__;
     }
     
     $unset_list = [];
+    $job_list = [];
     foreach ($docids as $key => $value) {
         if ($value['IS_EXISTS'] === false) {
             $unset_list[] = $key;
+        } else if ($value['FILE_SIZE'] === 0) {
+            $job_list[] = $key;
         }
     }
     foreach($unset_list as $key) {
         unset($docids[$key]);
     }
+    $unset_list = null;
+
+    error_log($log_prefix . 'total count : ' . count($job_list));
+    file_put_contents('/tmp/jobs.txt', implode("\n", $job_list));
+
+    $line = 'cat /tmp/jobs.txt | xargs -t -L 1 -P 2 -I{} '
+        . 'curl -sS -m 120 -w "(%{time_total}s %{size_download}b) " -D /tmp/zoho_{} -o /dev/null '
+        . "https://apidocs.zoho.com/files/v1/content/{}?authtoken=${authtoken_zoho}&scope=docsapi 2>/tmp/xargs_log.txt";
+    $res = null;
+    error_log($log_prefix . $line);
+    $time_start = microtime(true);
+    exec($line, $res);
+    $time_finish = microtime(true);
+    foreach ($res as $one_line) {
+        error_log($log_prefix . $one_line);
+    }
+    $res = null;
+    error_log($log_prefix . 'Error : ' . file_get_contents('/tmp/xargs_log.txt'));
+    error_log($log_prefix . 'Process Time : ' . substr(($time_finish - $time_start), 0, 6) . 's');
+    unlink('/tmp/jobs.txt');
+    unlink('/tmp/xargs_log.txt');
     
+    foreach ($job_list as $docid) {
+        $file_name = "/tmp/zoho_${docid}";
+        if (!file_exists($file_name) || filesize($file_name) === 0) {
+            error_log($log_prefix . 'File None : ' . $file_name);
+        } else {
+            $res = file_get_contents($file_name);
+            $rc = preg_match('/Content-Length: (\d+)/', $res, $match);
+            if ($rc === 1) {
+                error_log($log_prefix . $match[1] . ' : ' . trim($docid, "'"));
+                $size += (int)$match[1];
+                $docids[$docid]['FILE_SIZE'] = (int)$match[1];
+            }
+            unlink($docid);
+        }
+    }
     
+    error_log(print_r($docids, true));
+    error_log(strlen(base64_encode(bzcompress(serialize($docids)))));
     
+    $pdo = $mu_->get_pdo();
+    $statement_upsert = $pdo->prepare($sql_upsert);
     
-    
-    // error_log(print_r($docids, true));
-    
-    $tmp = base64_encode(bzcompress(serialize($docids)));
-    error_log(strlen($tmp));
-    
+    $rc = $statement_upsert->execute([':b_key' => 'apidocs.zoho.com',
+                                      ':b_value' => base64_encode(bzcompress(serialize($docids))),
+                                     ]);
+    error_log($log_prefix . 'UPSERT $rc : ' . $rc);
     
     $pdo = null;
     return;
