449935971d41ef4d7b5d20eff4453fb39be92f6d
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-29 15:35:37 +0900

    Create 20190829.php

---
+++ www/20190829.php
@@ -0,0 +1,23 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+$description = backup_etc($mu);
+
+$time_finish = microtime(true);
+$mu->post_blog_wordpress("${requesturi} [" . substr(($time_finish - $time_start), 0, 6) . 's]', $description);
+
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
+exit();
+
+function backup_etc($mu_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+}
