81f658b338489658628e1ad8bd261ad484a8e72c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-02 10:35:20 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -1255,8 +1255,10 @@ curl -v -m 120 -X PUT --compressed -T {$file_name_} --digest -u {$user_cloudme}:
 curl -v -m 120 -X PUT --compressed -T {$file_name_} -u {$user_4shared}:{$password_4shared} https://webdav.4shared.com/{$base_name}
 megaput -u {$user_mega} -p {$password_mega} --path /Root/{$base_name} {$file_name_}
 curl -v -m 120 -X POST --compressed -F filename={$base_name} -F content=@{$file_name_} https://apidocs.zoho.com/files/v1/upload?authtoken={$authtoken_zoho}&scope=docsapi
-curl -v -m 120 -X POST -H "Authorization: Bearer {$token_dropbox}" -H 'Dropbox-API-Arg: {"path": "{$base_name}.bz2", "mode": "overwrite", "autorename": false, "mute": false}' -H "Content-Type: application/octet-stream" --data-binary @{$file_name_}.bz2 https://content.dropboxapi.com/2/files/upload
 __HEREDOC__;
+        /*
+        curl -v -m 120 -X POST -H "Authorization: Bearer {$token_dropbox}" -H 'Dropbox-API-Arg: {"path": "{$base_name}.bz2", "mode": "overwrite", "autorename": false, "mute": false}' -H "Content-Type: application/octet-stream" --data-binary @{$file_name_}.bz2 https://content.dropboxapi.com/2/files/upload
+        */
 
         file_put_contents('/tmp/jobs.txt', $jobs);
         $line = 'cat /tmp/jobs.txt | parallel -j6 --joblog /tmp/joblog.txt 2>&1';
