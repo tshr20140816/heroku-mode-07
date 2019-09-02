3558923c842a46e04e6cfd890bb778753bda9603
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-02 10:49:46 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -113,8 +113,8 @@ done
 popd
 set -x
 
-$count1 = $(grep -c 'No syntax errors detected in' /tmp/php_error.txt)
-$count2 = $(wc -l /tmp/php_error.txt)
+count1 = $(grep -c 'No syntax errors detected in' /tmp/php_error.txt)
+count2 = $(wc -l /tmp/php_error.txt)
 
 if [ ${count1} -lt ${count2} ]; then
   curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/php_error_exists > /dev/null 2>&1
