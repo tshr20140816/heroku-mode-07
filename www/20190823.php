0d6abb04df33ed644ade122f95052be68e074c47
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-06 14:01:05 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -9,11 +9,170 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-func_20190823j($mu);
+func_20190823b($mu, '/tmp/dummy');
+@unlink('/tmp/dummy');
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 
-function func_20190823j($mu_)
+function func_20190823b($mu_, $file_name_rss_items_)
+{
+    $log_prefix = $mu_->logging_function_begin(__METHOD__);
+
+    for ($i = 0; $i < (int)date('t'); $i++) {
+        $labels[] = $i + 1;
+        $tmp = new stdClass();
+        $tmp->x = $i + 1;
+        $tmp->y = ((int)date('t') - $i) * 24;
+        $data1[] = $tmp;
+    }
+
+    $datasets = [];
+    $datasets[] = ['data' => $data1,
+                   'fill' => false,
+                   'lineTension' => 0,
+                   'pointStyle' => 'line',
+                   'backgroundColor' => 'black',
+                   'borderColor' => 'black',
+                   'borderWidth' => 1,
+                   'pointRadius' => 0,
+                   'label' => 'max',
+                  ];
+
+    $list = [['target' => 'toodledo',
+              'color' => 'green',
+             ],
+             ['target' => 'ttrss',
+              'color' => 'deepskyblue',
+             ],
+             ['target' => 'redmine',
+              'color' => 'blue',
+             ],
+             ['target' => 'first',
+              'color' => 'red',
+             ],
+             ['target' => 'kyoto',
+              'color' => 'orange',
+             ],
+             ['target' => 'toodledo2',
+              'color' => 'pink',
+             ],
+            ];
+
+    $sql = <<< __HEREDOC__
+SELECT T1.value
+  FROM t_data_log T1
+ WHERE T1.key = :b_key
+__HEREDOC__;
+
+    $pdo = $mu_->get_pdo();
+    $statement = $pdo->prepare($sql);
+
+    foreach ($list as $one_data) {
+        error_log(print_r($one_data, true));
+        $statement->execute([':b_key' => strtoupper($one_data['target'])]);
+        $result = $statement->fetchAll();
+        $quotas = json_decode($result[0]['value'], true);
+        error_log(print_r($quotas, true));
+
+        $data2 = [];
+        foreach ($quotas as $key => $value) {
+            $tmp = new stdClass();
+            $tmp->x = (int)substr($key, -2) - 1;
+            $tmp->y = (int)($value / 3600);
+            $data2[] = $tmp;
+        }
+
+        if (count($data2) < 3) {
+            return 0;
+        }
+        if ($data2[0]->x == 0) {
+            array_shift($data2);
+            $tmp = new stdClass();
+            $tmp->x = 1;
+            $tmp->y = 550;
+            $data2[0] = $tmp;
+        }
+
+        $datasets[] = ['data' => $data2,
+                       'fill' => false,
+                       'lineTension' => 0,
+                       'pointStyle' => 'circle',
+                       'backgroundColor' => $one_data['color'],
+                       'borderColor' => $one_data['color'],
+                       'borderWidth' => 2,
+                       'pointRadius' => 3,
+                       'pointBorderWidth' => 0,
+                       'label' => $one_data['target'],
+                      ];
+
+        $data3 = [];
+        $tmp = new stdClass();
+        $tmp->x = 1;
+        $tmp->y = 550;
+        $data3[] = $tmp;
+        $tmp = new stdClass();
+        $tmp->x = (int)date('t');
+        $tmp->y = 550 - (int)((550 - end($data2)->y) / end($data2)->x + 1) * (int)date('t');
+        $data3[] = $tmp;
+
+        $datasets[] = ['data' => $data3,
+                       'fill' => false,
+                       'lineTension' => 0,
+                       'backgroundColor' => $one_data['color'],
+                       'borderWidth' => 1,
+                       'borderColor' => $one_data['color'],
+                       'pointRadius' => 0,
+                       // 'label' => 'plan',
+                       'label' => '',
+                      ];
+    }
+
+    $pdo = null;
+
+    $scales = new stdClass();
+    $scales->xAxes[] = ['id' => 'x-axis-0',
+                        'ticks' => ['autoSkip' => false,
+                                    'fontSize' => 10,
+                                   ],
+                       ];
+
+    $json = ['type' => 'line',
+             'data' => ['labels' => $labels,
+                        'datasets' => $datasets,
+                       ],
+             'options' => ['legend' => ['display' => true,
+                                        'labels' => ['boxWidth' => 6,
+                                                     'fontColor' => 'black',
+                                                    ],
+                                       ],
+                           'animation' => ['duration' => 0,
+                                          ],
+                           'hover' => ['animationDuration' => 0,
+                                      ],
+                           'responsiveAnimationDuration' => 0,
+                           'annotation' => ['annotations' => [['type' => 'line',
+                                                               'mode' => 'vertical',
+                                                               'scaleID' => 'x-axis-0',
+                                                               'value' => count($datasets[1]['data']),
+                                                              ],
+                                                             ],
+                                           ],
+                           'scales' => $scales,
+                          ],
+            ];
+    $file = tempnam('/tmp', 'chartjs_' . md5(microtime(true)));
+    exec('node ../scripts/chartjs_node.js 600 320 ' . base64_encode(json_encode($json)) . ' ' . $file);
+    $res = file_get_contents($file);
+    unlink($file);
+
+    header('content-type: image/png');
+    echo $res;
+
+    error_log($log_prefix . 'END');
+    return 0;
+}
+
+function func_20190823a($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
