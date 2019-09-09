8a072ddafb04d897443c72d6bbfe9bc37d137822
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-09 10:42:23 +0900

    Update make_graph.php

--- www/make_graph.php
+++ www/make_graph.php
@@ -82,6 +82,216 @@ function make_score_map($mu_, $file_name_rss_items_)
     $color_index['ロッテ'] = 'black,silver';
     $color_index['楽天'] = 'darkred,orange';
 
+    $url = 'https://baseball.yahoo.co.jp/npb/standings/?4nocache' . date('Ymd', strtotime('+9 hours'));;
+    $res = $mu_->get_contents($url, null, true);
+
+    $tmp = explode('<table class="NpbPlSt yjM">', $res);
+
+    $rc = preg_match_all('/title="(.+?)"/', $tmp[1] . $tmp[2], $matches);
+
+    $list_team = $matches[1];
+
+    $rc = preg_match_all('/<td>(.+?)</', $tmp[1] . $tmp[2], $matches);
+
+    $gain_sum = 0;
+    $gain_min_value = 9999;
+    $gain_max_value = 0;
+    $loss_sum = 0;
+    $loss_min_value = 9999;
+    $loss_max_value = 0;
+    for ($i = 0; $i < 12; $i++) {
+        $gain = (int)$matches[1][$i * 13 + 7];
+        $loss = (int)$matches[1][$i * 13 + 8];
+
+        $gain_sum += $gain;
+        if ($gain_max_value < $gain) {
+            $gain_max_value = $gain;
+        }
+        if ($gain_min_value > $gain) {
+            $gain_min_value = $gain;
+        }
+
+        $loss_sum += $loss;
+        if ($loss_max_value < $loss) {
+            $loss_max_value = $loss;
+        }
+        if ($loss_min_value > $loss) {
+            $loss_min_value = $loss;
+        }
+    }
+    $loss_avg = round($loss_sum / 12);
+    $gain_avg = round($gain_sum / 12);
+    for ($i = 0; $i < 12; $i++) {
+        $tmp1 = new stdClass();
+        $tmp1->x = $matches[1][$i * 13 + 7];
+        $tmp1->y = $matches[1][$i * 13 + 8];
+        $tmp1->r = 7;
+        $tmp2 = [];
+        $tmp2[] = $tmp1;
+        $tmp3 = new stdClass();
+        $tmp3->label = $list_team[$i];
+        $tmp3->data = $tmp2;
+        $tmp3->backgroundColor = explode(',', $color_index[$list_team[$i]])[0];
+        $tmp3->borderWidth = 3;
+        $tmp3->borderColor = explode(',', $color_index[$list_team[$i]])[1];
+        $datasets[] = $tmp3;
+    }
+
+    $data2 = [];
+    $tmp1 = new stdClass();
+    $tmp1->x = $gain_min_value - ($gain_max_value - $gain_min_value);
+    $tmp1->y = $tmp1->x;
+    $data2[] = $tmp1;
+    $tmp1 = new stdClass();
+    $tmp1->x = $gain_max_value + ($gain_max_value - $gain_min_value);
+    $tmp1->y = $tmp1->x;
+    $data2[] = $tmp1;
+
+    $datasets[] = ['type' => 'scatter',
+                   'data' => $data2,
+                   'showLine' => true,
+                   'borderColor' => 'black',
+                   'borderWidth' => 1,
+                   'fill' => false,
+                   'pointRadius' => 0,
+                   'label' => '',
+                  ];
+
+    // error_log($log_prefix . print_r($datasets, true));
+
+    $scales = new stdClass();
+
+    $xaxis_max_value = $gain_max_value + (10 - $gain_max_value % 10);
+    $xaxis_max_value += $xaxis_max_value % 20 === 0 ? 20 : 10;
+
+    $xaxis_min_value = $gain_min_value - ($gain_min_value % 10);
+    $xaxis_min_value -= $xaxis_min_value % 20 === 0 ? 20 : 10;
+
+    $loss_max_value = $loss_max_value + (10 - $loss_max_value % 10);
+    $loss_max_value += $loss_max_value % 20 === 0 ? 20 : 10;
+
+    $loss_min_value = $loss_min_value - ($loss_min_value % 10);
+    $loss_min_value -= $loss_min_value % 20 === 0 ? 20 : 10;
+
+    $scales->xAxes[] = ['display' => true,
+                        'scaleLabel' => ['display' => true,
+                                         'labelString' => '得点',
+                                         'fontColor' => 'black',
+                                        ],
+                        'ticks' => ['max' => $xaxis_max_value,
+                                    'min' => $xaxis_min_value,
+                                    'stepSize' => 20,
+                                    'autoSkip' => false,
+                                   ],
+                       ];
+    $scales->yAxes[] = ['display' => true,
+                        'bottom' => $loss_min_value,
+                        'scaleLabel' => ['display' => true,
+                                         'labelString' => '失点',
+                                         'fontColor' => 'black',
+                                        ],
+                        'ticks' => ['max' => $loss_max_value,
+                                    'min' => $loss_min_value,
+                                    'stepSize' => 20,
+                                    'autoSkip' => false,
+                                   ],
+                       ];
+    $json = ['type' => 'bubble',
+             'data' => ['datasets' => $datasets],
+             'options' => ['legend' => ['position' => 'bottom',
+                                        'labels' => ['fontSize' => 10,
+                                                     'fontColor' => 'black',
+                                                     'padding' => 18,
+                                                    ],
+                                       ],
+                           'scales' => $scales,
+                           'annotation' => ['annotations' => [['type' => 'line',
+                                                               'mode' => 'vertical',
+                                                               'scaleID' => 'x-axis-0',
+                                                               'value' => $gain_avg,
+                                                               'borderColor' => 'black',
+                                                               'borderWidth' => 1,
+                                                              ],
+                                                              ['type' => 'line',
+                                                               'mode' => 'horizontal',
+                                                               'scaleID' => 'y-axis-0',
+                                                               'value' => $loss_avg,
+                                                               'borderColor' => 'black',
+                                                               'borderWidth' => 1,
+                                                              ],
+                                                             ],
+                                           ],
+                           'animation' => ['duration' => 0,],
+                           'hover' => ['animationDuration' => 0,],
+                           'responsiveAnimationDuration' => 0,
+                          ],
+            ];
+
+
+    $file = tempnam('/tmp', 'chartjs_' . md5(microtime(true)));
+    exec('node ../scripts/chartjs_node.js 640 360 ' . base64_encode(json_encode($json)) . ' ' . $file);
+    $res = file_get_contents($file);
+    unlink($file);
+
+    $im1 = imagecreatefromstring($res);
+
+    $im2 = imagecreatetruecolor(imagesx($im1), imagesy($im1) - 25);
+    imagefill($im2, 0, 0, imagecolorallocate($im2, 255, 255, 255));
+
+    imagecopy($im2, $im1, 0, 0, 0, 0, imagesx($im1), imagesy($im1) - 25);
+    imagedestroy($im1);
+
+    $file = tempnam('/tmp', 'png_' . md5(microtime(true)));
+    imagepng($im2, $file, 9);
+    imagedestroy($im2);
+
+    $res = file_get_contents($file);
+    unlink($file);
+
+    $description = '<img src="data:image/png;base64,' . base64_encode($res) . '" />';
+
+    $mu_->post_blog_hatena('Score Map', $description);
+    $mu_->post_blog_fc2_async('Score Map', $description);
+
+    $description = '<![CDATA[' . $description . ']]>';
+
+    $rss_item_text = <<< __HEREDOC__
+<item>
+<guid isPermaLink="false">__HASH__</guid>
+<pubDate>__PUBDATE__</pubDate>
+<title>Score Map</title>
+<link>http://dummy.local/</link>
+<description>__DESCRIPTION__</description>
+</item>
+__HEREDOC__;
+
+    $rss_item_text = str_replace('__PUBDATE__', date('D, j M Y G:i:s +0900', strtotime('+9 hours')), $rss_item_text);
+    $rss_item_text = str_replace('__DESCRIPTION__', $description, $rss_item_text);
+    $rss_item_text = str_replace('__HASH__', hash('sha256', $description), $rss_item_text);
+    file_put_contents($file_name_rss_items_, $rss_item_text, FILE_APPEND);
+
+    error_log($log_prefix . 'END');
+    return 0;
+}
+
+function make_score_map2($mu_, $file_name_rss_items_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+    error_log($log_prefix . 'BEGIN');
+
+    $color_index['広島'] = 'red,red';
+    $color_index['ヤクルト'] = 'cyan,yellowgreen';
+    $color_index['巨人'] = 'black,orange';
+    $color_index['ＤｅＮＡ'] = 'blue,blue';
+    $color_index['中日'] = 'dodgerblue,dodgerblue';
+    $color_index['阪神'] = 'yellow,yellow';
+    $color_index['西武'] = 'navy,navy';
+    $color_index['ソフトバンク'] = 'gold,black';
+    $color_index['日本ハム'] = 'darkgray,steelblue';
+    $color_index['オリックス'] = 'sandybrown,darkslategray';
+    $color_index['ロッテ'] = 'black,silver';
+    $color_index['楽天'] = 'darkred,orange';
+
     // $url = 'https://baseball.yahoo.co.jp/npb/standings/';
     $url = 'https://baseball.yahoo.co.jp/npb/standings/?4nocache' . date('Ymd', strtotime('+9 hours'));;
     $res = $mu_->get_contents($url, null, true);
@@ -1599,7 +1809,7 @@ function make_storage_usage($mu_, $file_name_rss_items_)
 
     unlink($file);
     */
-    
+
     $file = tempnam('/tmp', 'chartjs_' . md5(microtime(true)));
     exec('node ../scripts/chartjs_node.js 600 360 ' . base64_encode(json_encode($json)) . ' ' . $file);
     $res = file_get_contents($file);
