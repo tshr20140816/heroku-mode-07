465caeaca718c01681053d6ebe27456ff99a83eb
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-09 16:04:08 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -20,7 +20,8 @@ function func_20190823d($mu_)
     error_log($log_prefix . 'BEGIN');
     
     // $list_days = [3, 9, 10, 15, 16, 17, 18];
-    $list_days = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    // $list_days = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    $list_days = [4, 5, 6, 7, 8, 9, 10, 11, 12];
     // $list_days = [7];
     $list_cookie = [];
     $urls = [];
