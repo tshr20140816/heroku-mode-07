6554f1463d1b7c0c92fa58b9fed973f2db2567a1
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-09 12:38:06 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -55,7 +55,7 @@ function func_20190823d($mu_)
         'day=3',
         'hour=22',
         'minute=30',
-        'train=>5',
+        'train=5',
         'dep_stn=%89%AA%8ER',
         'arr_stn=%93%8C%8B%9E',
         'dep_stnpb=',
