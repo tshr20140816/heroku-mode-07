f192557664597731121c8b143a07c048c09e7680
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-11 11:41:15 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -37,7 +37,7 @@ function func_20190823e($mu_)
     # $line = "pixz -9 < ${file_name} > ${file_name}.xz";
     # $mu_->cmd_execute($line);
     
-    $line = "pxz -v -k ${file_name}";
+    $line = "pxz -v -k -9 ${file_name}";
     $mu_->cmd_execute($line);
     
     exec('ls -lang /tmp/', $res);
