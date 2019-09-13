1113efa4316988b158eb7e638cdb9f01e4d71418
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-13 15:27:42 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -14,6 +14,15 @@ func_20190823f($mu);
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 
+function func_20190823g($mu_)
+{
+    /*
+    https://greens.rwiths.net/r-withs/tfs0020a.do?hotelNo=736&GCode=greens&vipCode=&sort=1&curPage=1&f_lang=ja&ciDateY=2019&ciDateM=10&ciDateD=12&lowerCharge=0&upperCharge=999999&coDateY=2019&coDateM=10&coDateD=13&otona=2&s1=0&s2=0&y1=0&y2=0&y3=0&y4=0&room=1
+    https://greens.rwiths.net/r-withs/tfs0020a.do?hotelNo=9211&GCode=greens&vipCode=&sort=1&curPage=1&f_lang=ja&ciDateY=2019&ciDateM=10&ciDateD=11&lowerCharge=0&upperCharge=999999&coDateY=2019&coDateM=10&coDateD=12&otona=2&s1=0&s2=0&y1=0&y2=0&y3=0&y4=0&room=1
+    https://www.rj-win.jp/USER_PC/search/plan/group_id/81/hotel_id/76#start_position
+    */
+}
+
 function func_20190823f($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
