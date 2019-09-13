be6753b4b013391153debb585eaab79af65d7093
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-13 11:19:28 +0900

    Update search_hotel.php

--- www/search_hotel.php
+++ www/search_hotel.php
@@ -13,12 +13,13 @@ search_hotel($mu);
 // search_jtb_tour($mu);
 search_hotel_sancoinn($mu);
 
-$url = 'https://' . getenv('HEROKU_APP_NAME') . '.herokuapp.com/get_twitter_jaxa.php';
-exec('curl -u ' . getenv('BASIC_USER') . ':' . getenv('BASIC_PASSWORD') . " ${url} > /dev/null 2>&1 &");
+if (substr($_SERVER['HTTP_USER_AGENT'], 0, 4) === 'curl') {
+    $url = 'https://' . getenv('HEROKU_APP_NAME') . '.herokuapp.com/get_twitter_jaxa.php';
+    exec('curl -u ' . getenv('BASIC_USER') . ':' . getenv('BASIC_PASSWORD') . " ${url} > /dev/null 2>&1 &");
 
-$url = 'https://' . getenv('HEROKU_APP_NAME') . '.herokuapp.com/lib_info.php?n=0';
-exec('curl -u ' . getenv('BASIC_USER') . ':' . getenv('BASIC_PASSWORD') . " ${url} > /dev/null 2>&1 &");
-
+    $url = 'https://' . getenv('HEROKU_APP_NAME') . '.herokuapp.com/lib_info.php?n=0';
+    exec('curl -u ' . getenv('BASIC_USER') . ':' . getenv('BASIC_PASSWORD') . " ${url} > /dev/null 2>&1 &");
+}
 $time_finish = microtime(true);
 
 $mu->post_blog_wordpress_async("${requesturi} [" . substr(($time_finish - $time_start), 0, 6) . 's]');
