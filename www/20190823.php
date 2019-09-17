abf1d7976bc2ecdad33d4fdd6a6287e3e01f6e65
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-18 08:55:39 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -16,7 +16,7 @@ error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's'
 
 function func_20190823j($mu_)
 {
-    $res = $mu_->cmd_execute('curl -v | head -n 1');
+    $res = $mu_->cmd_execute('curl -V | head -n 1');
     $version_current = trim(str_replace(["\r\n", "\r", "\n", '   ', '  '], ' ', $res[0]));
     error_log(print_r($res, true));
 }
