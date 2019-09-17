3c9c29ecc81a1e5315c335c24a6422f43217c27f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-17 17:22:11 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -9,6 +9,9 @@ curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_000 >/dev/null
 grep -c -e processor /proc/cpuinfo
 cat /proc/cpuinfo | head -n $(($(cat /proc/cpuinfo | wc -l) / $(grep -c -e processor /proc/cpuinfo)))
 
+whereis gcc
+gcc --version
+
 # ***** XML_RPC2 *****
 
 pear config-show
