8e3faa8c963ed8bdf7759ef05f7c88e4bc7bb53d
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-17 17:17:31 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,7 +4,7 @@ set -x
 
 date
 
-curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_000 > /dev/null 2>&1 &
+curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_000 >/dev/null 2>&1 &
 
 grep -c -e processor /proc/cpuinfo
 cat /proc/cpuinfo | head -n $(($(cat /proc/cpuinfo | wc -l) / $(grep -c -e processor /proc/cpuinfo)))
@@ -135,7 +135,7 @@ count1=$(grep -c 'No syntax errors detected in' /tmp/php_error.txt)
 count2=$(cat /tmp/php_error.txt | wc -l)
 
 if [ $count1 -lt $count2 ]; then
-  curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/php_error_exists > /dev/null 2>&1
+  curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/php_error_exists >/dev/null 2>&1
 fi
 
 cp /tmp/php_error.txt ./
@@ -157,12 +157,12 @@ rm -f *.zip
 popd
 ls -lang .fonts/
 
-bin/curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_100 > /dev/null 2>&1
+bin/curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_100 >/dev/null 2>&1
 
 date
 
 wait
 
-bin/curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_200 > /dev/null 2>&1
+bin/curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_200 >/dev/null 2>&1
 
 date
