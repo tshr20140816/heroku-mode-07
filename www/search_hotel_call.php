1c5d2259734598b64802ead44448463c763623e8
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-17 12:31:26 +0900

    Update search_hotel_call.php

--- www/search_hotel_call.php
+++ www/search_hotel_call.php
@@ -9,6 +9,8 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 $mu = new MyUtils();
 
 $url = 'https://' . getenv('HEROKU_APP_NAME') . '.herokuapp.com/search_hotel.php';
-exec('curl -m 3 -u ' . getenv('BASIC_USER') . ':' . getenv('BASIC_PASSWORD') . ' -H "User-Agent: search_hotel_call.php"' . " ${url} > /dev/null 2>&1 &");
+$line = 'curl -m 3 -u ' . getenv('BASIC_USER') . ':' . getenv('BASIC_PASSWORD') . ' -H "User-Agent: search_hotel_call.php"' . " ${url} > /dev/null 2>&1 &";
+error_log("${pid} ${line}");
+exec($line);
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
