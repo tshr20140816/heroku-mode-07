5b2c4307de74cc1f299295a3b345646c392c4140
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-18 10:13:41 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -6,6 +6,8 @@ date
 
 curl -s -m 1 https://${HEROKU_APP_NAME}.herokuapp.com/check_point_000 >/dev/null 2>&1 &
 
+tree -a .apt
+
 grep -c -e processor /proc/cpuinfo
 cat /proc/cpuinfo | head -n $(($(cat /proc/cpuinfo | wc -l) / $(grep -c -e processor /proc/cpuinfo)))
 
