0524731d9664561a87b03f7230672c4d91e7ed16
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-19 10:43:34 +0900

    Update daily030.php

--- www/daily030.php
+++ www/daily030.php
@@ -154,6 +154,35 @@ for ($yyyy = (int)date('Y'); $yyyy < (int)date('Y') + 2; $yyyy++) {
     $res = $mu->get_contents('http://www.calc-site.com/calendars/solar_year', $options, true);
 }
 
+$cookie = tempnam('/tmp', 'cookie_' . md5(microtime(true)));
+$options = [CURLOPT_ENCODING => 'gzip, deflate',
+            CURLOPT_HTTPHEADER => [
+                'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
+                'Cache-Control: no-cache',
+                'Connection: keep-alive',
+                'DNT: 1',
+                'Upgrade-Insecure-Requests: 1',
+                ],
+            CURLOPT_COOKIEJAR => $cookie,
+            CURLOPT_COOKIEFILE => $cookie,
+            CURLOPT_HEADER => true,
+           ];
+
+$url_base = 'https://www.accuweather.com/ja/jp/hiroshima-shi/223955/daily-weather-forecast/223955';
+
+$urls = [];
+$urls[] = $url_base;
+$urls[] = $url_base . '?page=1';
+$urls[] = $url_base . '?page=2';
+$urls[] = $url_base . '?page=3';
+$urls[] = $url_base . '?page=4';
+
+foreach ($urls as $url) {
+    $res = $mu->get_contents($url, $options, true);
+}
+unlink($cookie);
+
 $url = 'https://' . getenv('HEROKU_APP_NAME') . '.herokuapp.com/get_youtube_play_count.php';
 exec('curl -u ' . getenv('BASIC_USER') . ':' . getenv('BASIC_PASSWORD') . " ${url} > /dev/null 2>&1 &");
 
