abf4a921620c3aeaba78459d57ac3fa6465cb7d5
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-19 12:43:42 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -62,7 +62,7 @@ function func_20190823h($mu_)
     
     $rc = preg_match('/<td align="left">Ｍａｘとき３０７号２階<\/td>.+?<{5}.+?>(.+?)</s', $res, $match);
     
-    error_log(print_r($res, true));
+    error_log(print_r($match, true));
 }
 
 function func_20190823g($mu_)
