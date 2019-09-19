9294be8856c5fde6f7dba3378c29789b8d4179e5
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-19 11:56:41 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -60,6 +60,8 @@ function func_20190823h($mu_)
     unlink($cookie);
     $res = mb_convert_encoding($res, 'UTF-8', 'SJIS');
     
+    $rc = preg_match('/<td align="left">Ｍａｘとき３０７号２階<\/td>/s', $res, $match);
+    
     error_log($res);
 }
 
