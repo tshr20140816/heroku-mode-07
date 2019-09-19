bfe6df11b0897965286c38ac731017d55cc45ddc
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-19 14:06:21 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -10,7 +10,7 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-func_20190823i($mu);
+func_20190823h($mu);
 // @unlink('/tmp/dummy');
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
