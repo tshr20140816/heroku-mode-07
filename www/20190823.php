0c7106bcd4e04010a1c667ceb0df552e47e5f632
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-24 09:24:29 +0900

    Update 20190823.php

--- www/20190823.php
+++ www/20190823.php
@@ -63,7 +63,7 @@ function func_20190823l($mu_)
         asort($hotels);
         // error_log($log_prefix . print_r($hotels, true));
         $hotels = array_chunk($hotels, 28, true);
-        // error_log($log_prefix . print_r($hotels, true));
+        error_log($log_prefix . print_r($hotels, true));
         $info .= implode("\n", array_keys($hotels));
         error_log($log_prefix. $info);
     }
