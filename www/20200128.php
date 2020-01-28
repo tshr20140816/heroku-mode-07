3f79239202e43f457928c84a9c06012461948e5e
tshr20140816 <tshr20140816@yahoo.co.jp>
2020-01-28 09:04:35 +0900

    Update 20200128.php

--- www/20200128.php
+++ www/20200128.php
@@ -21,5 +21,35 @@ function f20200128($mu_)
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
     error_log($log_prefix . 'BEGIN');
 
+    $url = $mu_->get_env('URL_OUTLET');
+    $res = $mu_->get_contents($url);
+    
+    $rc = preg_match('/<p id="parkingnow"><img src="(.+?)"/s', $res, $matches);
+    $res = $mu_->get_contents($matches[1]);
+    $hash_text = hash('sha512', $res);
 
+    $pdo = $mu_->get_pdo();
+
+    $sql = <<< __HEREDOC__
+SELECT T1.parse_text
+  FROM t_imageparsehash T1
+ WHERE T1.group_id = 1
+   AND T1.hash_text = :b_hash_text;
+__HEREDOC__;
+
+    $statement = $pdo->prepare($sql);
+    $rc = $statement->execute([':b_hash_text' => $hash_text]);
+    error_log($log_prefix . "SELECT RESULT : ${rc}");
+    $results = $statement->fetchAll();
+    error_log($log_prefix . '$results : ' . print_r($results, true));
+
+    $parse_text = '';
+    foreach ($results as $row) {
+        $parse_text = $row['parse_text'];
+        break;
+    }
+
+    $pdo = null;
+    
+    error_log($parse_text);
 }
