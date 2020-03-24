491d2d6711f763ab5f1fd34df7629e0fa060200b
tshr20140816 <tshr20140816@yahoo.co.jp>
2020-03-24 10:03:41 +0900

    Update daily010.php

--- www/daily010.php
+++ www/daily010.php
@@ -20,7 +20,7 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$url = 'https://' . getenv('HEROKU_APP_NAME') . '.herokuapp.com/daily020.php';
+$url = 'https://' . getenv('HEROKU_APP_NAME') . '.herokuapp.com/daily020.php?index=100';
 exec('curl -u ' . getenv('BASIC_USER') . ':' . getenv('BASIC_PASSWORD') . " ${url} > /dev/null 2>&1 &");
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
